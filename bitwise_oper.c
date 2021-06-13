e0dd32e6eee21195bc49c311e61b31e299475448
root <ivanovlm@lion>
2021-06-13 15:07:56 +0700

    rel v1

--- bitwise_oper.c
+++ bitwise_oper.c
@@ -5,8 +5,17 @@ int main (void)
 	int a = 0x0000DDAABB;
 	unsigned char c;
 
-	c = (a >> 4);
-	printf("%x\n", c);
+	c = (a & 0xF);
+	printf("%x\n", c);//0xB
+	a = (a >> 4);
+	c = (a & 0xF);
+	printf("%x\n", c);//0xB
+	a = (a >> 4);
+	c = (a & 0xF);
+	printf("%x\n", c);//0xA
+	a = (a >> 4);
+	c = (a & 0xF);
+	printf("%x\n", c);//0xA
 
 	return 0;
 }
